0b35a4cdce4098d45ed74852e24f45e597239be7
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-29 20:58:35 -0600

    Used Hashtable to reduce adding and removing time
    O(1) here we go

--- index.js
+++ index.js
@@ -2,29 +2,61 @@ var app = require('express')()
 var http = require('http').createServer(app)
 var io = require('socket.io')(http)
 
-let rooms = [{ name: 'Waiting Room', count: 0 },
-{ name: 'Delta', count: 0 },
-{ name: 'Charlie', count: 0 }
-    , { name: 'Dingo', count: 0 }]
-
+let rooms = []
 let players = []
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+debugGetAllPlayers = () => {
+    var Allplayers = []
+    for (player in players) {
+        Allplayers.push(players[player])
+    }
+    return Allplayers
+}
+
 app.get('/debug', (req, res) => {
     res.send({
-        users: players,
-        rooms
+        rooms,
+        players: debugGetAllPlayers()
     })
 })
 
+getRoomToConnect = async () => {
+    var room = {}
+    // If no rooms exist, create a new room
+    if (rooms.length == 0) {
+        room.name = uuidv4()
+        room.count = 0
+        room.open = true
+        return rooms.push(room) - 1
+    }
+
+    // If rooms exist, check if one is open
+    for (const [i, room] of rooms.entries()) {
+        if(room.open) { return i }
+    }
+
+    //If none is, create a new one
+    room.name = uuidv4()
+    room.count = 0
+    room.open = true
+    return rooms.push(room) - 1
+}
+
 io.on('connection', async socket => {
-    await onRoomConnection(socket, 0)
-    await onPlayerCreation(socket.id, 0)
+    let roomIndex = await getRoomToConnect()
+    await onRoomConnection(socket,roomIndex)
+    await onPlayerCreation(socket.id, roomIndex)
     socket.on('disconnect', async () => {
         await onRoomDisconnect(socket)
+    })
+
+    socket.on('chat message', (msg) => {
+        let player = players.find(player => player.socketId == socket.id)
+        socket.to(player.room).emit('message', msg)
     })
 })
 
@@ -34,33 +66,31 @@ http.listen(3000, () => {
 
 onRoomDisconnect = async (socket) => {
     console.log(`${socket.id} disconnected`)
-    players = players.filter(player => {
-        if (player.socketId == socket.id) {
-            rooms.find(room => {
-                if (room.name == player.room) { room.count -= 1 }
-            })
-            return false
-        } else {
-            return true
-        }
+    rooms[players[socket.id].roomIdx].count -= 1
+    delete players[socket.id]
+}
+
+uuidv4 = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+
+onRoomConnection = async (socket,index) => {
+    socket.join(rooms[index].name, () => {
+        rooms[index].count += 1
+        if(rooms[index].count >= 15) { rooms[index].open = false }
+        console.log(`${rooms[index].count} Townsies in ${rooms[index].name}`)
     })
 }
 
-onRoomConnection = async (socket, roomIndex) => {
-    socket.join(rooms[roomIndex].name, () => {
-        rooms[roomIndex].count += 1
-        console.log(`${rooms[0].count} Townsies in ${rooms[0].name}`)
-    })
-}
-
-onPlayerCreation = async (socketId, roomIndex) => {
-    const player = {
-        name: `Townie ${players.length + 1}`,
-        room: rooms[roomIndex].name,
-        socketId
+onPlayerCreation = async (socketId, roomIdx) => {
+    players[socketId] = {
+        name: `Townie ${socketId}`,
+        roomIdx
     }
-    players.push(player)
-
-    console.log(`connected to ${player.room}`)
-    console.log(`Welcome to townsies ${player.name}`)
+    console.log(`connected to ${rooms[roomIdx].name}`)
+    console.log(`Welcome to townsies ${players[socketId].name}`)
 }
