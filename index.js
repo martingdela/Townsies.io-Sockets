8b7d24384098a9cc666c51285c396eaf26fba750
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-29 22:13:20 -0600

    Improved connection and disconnection functions
    Now Joining and leaving behaves as expected

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var io = require('socket.io')(http)
 
 let rooms = []
 let players = []
+const MAX_PLAYERS_PER_ROOM = 2
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -25,13 +26,9 @@ app.get('/debug', (req, res) => {
 })
 
 getRoomToConnect = async () => {
-    var room = {}
     // If no rooms exist, create a new room
     if (rooms.length == 0) {
-        room.name = uuidv4()
-        room.count = 0
-        room.open = true
-        return rooms.push(room) - 1
+        return createNewRoom()
     }
 
     // If rooms exist, check if one is open
@@ -40,7 +37,12 @@ getRoomToConnect = async () => {
     }
 
     //If none is, create a new one
-    room.name = uuidv4()
+    return createNewRoom()
+}
+
+createNewRoom = (name) => {
+    var room = {}
+    room.name = name ? name : uuidv4()
     room.count = 0
     room.open = true
     return rooms.push(room) - 1
@@ -50,13 +52,14 @@ io.on('connection', async socket => {
     let roomIndex = await getRoomToConnect()
     await onRoomConnection(socket,roomIndex)
     await onPlayerCreation(socket.id, roomIndex)
+
     socket.on('disconnect', async () => {
         await onRoomDisconnect(socket)
     })
 
-    socket.on('chat message', (msg) => {
-        let player = players.find(player => player.socketId == socket.id)
-        socket.to(player.room).emit('message', msg)
+    socket.on('chat-message', (msg) => {
+        if(msg.substring(0,1) == '/') { return onCommandInput(msg,socket) } 
+        socket.to(rooms[players[socket.id].roomIdx].name).emit('message', msg)
     })
 })
 
@@ -66,8 +69,13 @@ http.listen(3000, () => {
 
 onRoomDisconnect = async (socket) => {
     console.log(`${socket.id} disconnected`)
-    rooms[players[socket.id].roomIdx].count -= 1
+    onRoomLeaving(socket)
     delete players[socket.id]
+}
+
+onRoomLeaving = socket => {
+    rooms[players[socket.id].roomIdx].count -= 1
+    if (rooms[players[socket.id].roomIdx].count < MAX_PLAYERS_PER_ROOM) { rooms[players[socket.id].roomIdx].open = true }
 }
 
 uuidv4 = () => {
@@ -77,11 +85,43 @@ uuidv4 = () => {
     });
 }
 
+onCommandInput = (msg,socket) => {
+    //Remove the first slash
+    msg = msg.substring(1,msg.length)
+    msg = msg.split(' ')
+    if(msg.length < 2) { return console.log(`Expected args (2) got ${msg.length}. Won't parse`)}
+    switch(msg[0]) {
+        case 'switch':
+            console.log(`player is in room: ${players[socket.id].roomIdx}`)
+            let idx = -1
+            for (const [i, room] of rooms.entries()) {
+                if(room.name == msg[1]) { 
+                    idx = i
+                }
+            }
+            
+            if(idx < 0) { idx = createNewRoom(msg[1]) }
+            if(!rooms[idx].open) { return console.log('Room is at capacity')}
+
+            socket.leave(rooms[players[socket.id].roomIdx].name, () => {
+                onRoomLeaving(socket)
+                onRoomConnection(socket,idx)
+                players[socket.id].roomIdx = idx
+            })
+            break
+        case 'nickname':
+            players[socket.id].name = msg.slice(1).join(" ")
+            break
+        default:
+            console.log('this command doesnt exist')
+    }
+}
+
 
 onRoomConnection = async (socket,index) => {
     socket.join(rooms[index].name, () => {
         rooms[index].count += 1
-        if(rooms[index].count >= 15) { rooms[index].open = false }
+        if(rooms[index].count >= MAX_PLAYERS_PER_ROOM) { rooms[index].open = false }
         console.log(`${rooms[index].count} Townsies in ${rooms[index].name}`)
     })
 }
