308a98777bc9a9e7bad0b639552ead815e05ba03
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-29 20:03:23 -0600

    Solved connection and disconnection to room issues

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ var app = require('express')()
 var http = require('http').createServer(app)
 var io = require('socket.io')(http)
 
-let rooms = [{name: 'Waiting Room', count: 0},
-            {name: 'Delta', count: 0},
-            {name: 'Charlie', count: 0}
-            ,{name: 'Dingo', count: 0}]
+let rooms = [{ name: 'Waiting Room', count: 0 },
+{ name: 'Delta', count: 0 },
+{ name: 'Charlie', count: 0 }
+    , { name: 'Dingo', count: 0 }]
 
 let players = []
 
-app.get('/',(req,res) => {
-    res.sendFile(__dirname+'/index.html')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
 })
 
-app.get('/debug',(req,res) => {
+app.get('/debug', (req, res) => {
     res.send({
         users: players,
         rooms
@@ -21,29 +21,46 @@ app.get('/debug',(req,res) => {
 })
 
 io.on('connection', async socket => {
-    socket.join(rooms[0].name, () => {
-        rooms[0].count += 1
-        console.log(`${rooms[0].count} Townsies in ${rooms[0].name}`)
-    })
-    
-    await newConnection(0,socket.id)
-    socket.on('disconnect', () => {
-        console.log(`${socket.id} disconnected`)
+    await onRoomConnection(socket, 0)
+    await onPlayerCreation(socket.id, 0)
+    socket.on('disconnect', async () => {
+        await onRoomDisconnect(socket)
     })
 })
 
-http.listen(3000,() => {
+http.listen(3000, () => {
     console.log('Listening on *:3000')
 })
 
-newConnection = async (room, socketId) => {
+onRoomDisconnect = async (socket) => {
+    console.log(`${socket.id} disconnected`)
+    players = players.filter(player => {
+        if (player.socketId == socket.id) {
+            rooms.find(room => {
+                if (room.name == player.room) { room.count -= 1 }
+            })
+            return false
+        } else {
+            return true
+        }
+    })
+}
+
+onRoomConnection = async (socket, roomIndex) => {
+    socket.join(rooms[roomIndex].name, () => {
+        rooms[roomIndex].count += 1
+        console.log(`${rooms[0].count} Townsies in ${rooms[0].name}`)
+    })
+}
+
+onPlayerCreation = async (socketId, roomIndex) => {
     const player = {
         name: `Townie ${players.length + 1}`,
-        room : rooms[0].name,
+        room: rooms[roomIndex].name,
         socketId
     }
     players.push(player)
-    
+
     console.log(`connected to ${player.room}`)
     console.log(`Welcome to townsies ${player.name}`)
 }
